f21dfdc3bb0347109d58f45007c98bd1613a5bef
GBaudelaire <germainjones1@gmail.com>
2025-12-05 16:30:31 +0000

    Add Custom waybar Update hyprland configuration Add default LazyVim configuration

---
+++ .config/nvim/lua/plugins/cssls.lua
@@ -0,0 +1,28 @@
+-- return {
+--   {
+--   "neovim/nvim-lspconfig",
+--   opts = {
+--     servers = {
+--       cssls = {
+--         settings = {
+--           css = {
+--             lint = {
+--               unknownAtRules = "ignore",
+--             },
+--           },
+--           scss = {
+--             lint = {
+--               unknownAtRules = "ignore",
+--             },
+--           },
+--           less = {
+--             lint = {
+--               unknownAtRules = "ignore",
+--             },
+--           },
+--         },
+--       },
+--     },
+--   },
+-- },
+-- }
